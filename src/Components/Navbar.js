3fe486623aa3417e8eedfd18ab2125775c914f76
Minhajulislam0 <mdmishu98730@gmail.com>
2023-03-10 03:31:30 +0600

    Sign in

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -12,35 +12,37 @@ export default function Navbar() {
   }
 
   return (
-    <div className="bg-white border-b shadow-lg static top-0 z-50">
+    <div className="bg-white border-b shadow-sm static top-0 z-50">
       <header className="flex justify-between items-center px-3 max-w-6xl mx-auto">
         <div
           className=" cursor-pointer text-2xl font-bold  text-violet-500"
           onClick={() => navigate("/")}
         >
-          Real<span className=" text-red-400">State</span>
+          Real<span className="text-red-400">State</span>
         </div>
         <div>
-          <ul className="flex space-x-10">
+          <ul className="flex space-x-10 text-gray-400">
             <li
-              className={`cursor-pointer py-3 text-sm font-semibold text-gray-400 border-b-[3px] border-b-transparent ${
-                pathMarker("/") && "text-black border-b-red-500"
+              className={` cursor-pointer py-3 text-sm font-semibold  ${
+                pathMarker("/") && "border-b-[3px] text-black border-b-red-500"
               } `}
               onClick={() => navigate("/")}
             >
               Home
             </li>
             <li
-              className={`cursor-pointer py-3 text-sm font-semibold text-gray-400 border-b-[3px] border-b-transparent ${
-                pathMarker("/offers") && "text-black border-b-red-500"
+              className={`cursor-pointer py-3 text-sm font-semibold ${
+                pathMarker("/offers") &&
+                "border-b-[3px] text-black border-b-red-500"
               } `}
               onClick={() => navigate("/offers")}
             >
-              Offer
+              Offers
             </li>
             <li
-              className={`cursor-pointer py-3 text-sm font-semibold text-gray-400 border-b-[3px] border-b-transparent ${
-                pathMarker("/sign-in") && "text-black border-b-red-500"
+              className={`cursor-pointer py-3 text-sm font-semibold ${
+                pathMarker("/sign-in") &&
+                "border-b-[3px] text-black border-b-red-500"
               } `}
               onClick={() => navigate("/sign-in")}
             >
