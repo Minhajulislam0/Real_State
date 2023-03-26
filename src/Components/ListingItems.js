cd207208fe6d8a3322efb3fe1945d2746f2cd4dd
Minhajulislam0 <mdmishu98730@gmail.com>
2023-03-27 01:37:32 +0600

    Moment added

--- src/Components/ListingItems.js
+++ src/Components/ListingItems.js
@@ -1,5 +1,21 @@
 import React from "react";
+import Moment from "react-moment";
+import { Link } from "react-router-dom";
+import { MdLocationPin } from "react-icons/md";
 
 export default function ListingItems({ listing, id }) {
-  return <div>{listing.name}</div>;
+  return (
+    <li>
+      <Link to={`/category/${listing.type}/${id}`}>
+        <img src={listing.imgUrls[0]} alt="" />
+        <Moment fromNow>{listing.timestamp?.toDate()}</Moment>
+        <div>
+          <div>
+            <MdLocationPin />
+            <p>{listing.address}</p>
+          </div>
+        </div>
+      </Link>
+    </li>
+  );
 }
