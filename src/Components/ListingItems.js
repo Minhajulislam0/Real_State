69ebdfe8c95e0e6b0707dbadbfed855bb3fa8f49
Minhajulislam0 <mdmishu98730@gmail.com>
2023-03-27 02:24:01 +0600

    Listing Items Completed

--- src/Components/ListingItems.js
+++ src/Components/ListingItems.js
@@ -5,14 +5,51 @@ import { MdLocationPin } from "react-icons/md";
 
 export default function ListingItems({ listing, id }) {
   return (
-    <li>
-      <Link to={`/category/${listing.type}/${id}`}>
-        <img src={listing.imgUrls[0]} alt="" />
-        <Moment fromNow>{listing.timestamp?.toDate()}</Moment>
-        <div>
-          <div>
-            <MdLocationPin />
-            <p>{listing.address}</p>
+    <li className="relative bg-white flex flex-col justify-between items-center shadow-md hover:shadow-xl rounded-md overflow-hidden transition-shadow duration-150 m-[10px]">
+      <Link className="contents" to={`/category/${listing.type}/${id}`}>
+        <img
+          className="h-[170px] w-full object-cover hover:scale-105 transition-scale duration-200 ease-in"
+          loading="lazy"
+          src={listing.imgUrls[0]}
+        />
+        <Moment
+          className=" absolute top-2 left-2 bg-[#b3b824] text-white uppercase text-xs font-semibold rounded-md px-2 py-1 shadow-lg"
+          fromNow
+        >
+          {listing.timestamp?.toDate()}
+        </Moment>
+        <div className=" w-full p-[10px]">
+          <div className="flex items-center space-x-1">
+            <MdLocationPin className="h-3 w-4 text-yellow-500" />
+            <p className=" font-semibold text-sm mb-[2px] text-gray-600">
+              {listing.address}
+            </p>
+          </div>
+          <p className="font-semibold m-0 text-xl truncate">{listing.name}</p>
+          <p className="text-[#457b9d] mt-2 font-semibold">
+            $
+            {listing.offer
+              ? listing.discountedPrice
+                  .toString()
+                  .replace(/\B(?=(\d{3})+(?!\d))/g, ",")
+              : listing.regularPrice
+                  .toString()
+                  .replace(/\B(?=(\d{3})+(?!\d))/g, ",")}
+            {listing.type === "rent" && " / month"}
+          </p>
+          <div className="flex items-center mt-[10px] space-x-3">
+            <div className="flex items-center space-x-1">
+              <p className="font-bold text-xs">
+                {listing.bedrooms > 1 ? `${listing.bedrooms} Beds` : "1 Bed"}
+              </p>
+            </div>
+            <div className="flex items-center space-x-1">
+              <p className="font-bold text-xs">
+                {listing.bathrooms > 1
+                  ? `${listing.bathrooms} Baths`
+                  : "1 Bath"}
+              </p>
+            </div>
           </div>
         </div>
       </Link>
