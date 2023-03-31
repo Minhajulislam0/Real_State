e4949a95d8fd6944d76c4ca6035b7d98567d2c68
Minhajulislam0 <mdmishu98730@gmail.com>
2023-04-01 03:09:30 +0600

    Added map & leaflet and react-leaflet

--- src/pages/ListingPage.js
+++ src/pages/ListingPage.js
@@ -21,6 +21,7 @@ import {
 } from "react-icons/fa";
 import { getAuth } from "firebase/auth";
 import Contact from "../Components/Contact";
+import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
 
 export default function ListingPage() {
   const auth = getAuth();
@@ -150,7 +151,26 @@ export default function ListingPage() {
           )}
         </div>
 
-        <div className="bg-green-300 w-full h-[200px] lg-[400px] z-10 overflow-x-hidden"></div>
+        <div className="w-full z-10 overflow-x-hidden">
+          <MapContainer
+            center={[listing.geolocation.lat, listing.geolocation.lng]}
+            zoom={13}
+            scrollWheelZoom={false}
+            height={{ height: "100%", width: "100%" }}
+          >
+            <TileLayer
+              attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+              url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            />
+            <Marker
+              position={[listing.geolocation.lat, listing.geolocation.lng]}
+            >
+              <Popup>
+                A pretty CSS3 popup. <br /> Easily customizable.
+              </Popup>
+            </Marker>
+          </MapContainer>
+        </div>
       </div>
     </main>
   );
