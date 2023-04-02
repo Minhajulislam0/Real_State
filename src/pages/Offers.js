5e15f0ce090400b68ad16a9ab0ec53a485d7dfe6
Minhajulislam0 <mdmishu98730@gmail.com>
2023-04-03 03:12:52 +0600

    Offers Added

--- src/pages/Offers.js
+++ src/pages/Offers.js
@@ -1,5 +1,73 @@
 import React from "react";
+import { useEffect } from "react";
+import { useState } from "react";
+import { toast } from "react-toastify";
+import {
+  collection,
+  getDocs,
+  limit,
+  orderBy,
+  query,
+  where,
+} from "firebase/firestore";
+import { db } from "./../FireBase";
+import Spinner from "../Components/Spinner";
+import ListingItems from "../Components/ListingItems";
 
 export default function Offers() {
-  return <div>Offers</div>;
+  const [listings, setListings] = useState(null);
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    async function fetchListings() {
+      try {
+        // get ref
+        const listingRef = collection(db, "listings");
+        // create query
+        const q = query(
+          listingRef,
+          where("offer", "==", true),
+          orderBy("timestamp", "desc"),
+          limit(8)
+        );
+        // execute query
+        const querySnap = await getDocs(q);
+        const listings = [];
+        querySnap.forEach((doc) => {
+          return listings.push({
+            id: doc.id,
+            data: doc.data(),
+          });
+        });
+        setListings(listings);
+        setLoading(false);
+      } catch (error) {
+        toast.error("Offer Could not Load..");
+      }
+    }
+    fetchListings();
+  }, []);
+  return (
+    <div className="max-w-6xl mx-auto px-3">
+      <h1 className="text-3xl text-center mt-6 font-bold">Offers</h1>
+      {loading ? (
+        <Spinner />
+      ) : listings && listings.length > 0 ? (
+        <>
+          <main>
+            <ul className="sm:grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5">
+              {listings.map((listing) => (
+                <ListingItems
+                  key={listing.id}
+                  listing={listing.data}
+                  id={listing.id}
+                />
+              ))}
+            </ul>
+          </main>
+        </>
+      ) : (
+        <p>There Are No Current Offers!</p>
+      )}
+    </div>
+  );
 }
