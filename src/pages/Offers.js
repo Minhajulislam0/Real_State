4b4edbe785daa6a365a9aa2c6e11bccf42ef4f4f
Minhajulislam0 <mdmishu98730@gmail.com>
2023-04-03 03:34:57 +0600

    Complete Offers page

--- src/pages/Offers.js
+++ src/pages/Offers.js
@@ -8,6 +8,7 @@ import {
   limit,
   orderBy,
   query,
+  startAfter,
   where,
 } from "firebase/firestore";
 import { db } from "./../FireBase";
@@ -17,6 +18,7 @@ import ListingItems from "../Components/ListingItems";
 export default function Offers() {
   const [listings, setListings] = useState(null);
   const [loading, setLoading] = useState(true);
+  const [lastFetchListing, setLastFetchListing] = useState(null);
   useEffect(() => {
     async function fetchListings() {
       try {
@@ -27,10 +29,12 @@ export default function Offers() {
           listingRef,
           where("offer", "==", true),
           orderBy("timestamp", "desc"),
-          limit(8)
+          limit(5)
         );
         // execute query
         const querySnap = await getDocs(q);
+        const laseVisable = querySnap.docs[querySnap.docs.length - 1];
+        setLastFetchListing(laseVisable);
         const listings = [];
         querySnap.forEach((doc) => {
           return listings.push({
@@ -46,9 +50,40 @@ export default function Offers() {
     }
     fetchListings();
   }, []);
+
+  async function onFetchMoreListings() {
+    try {
+      // get ref
+      const listingRef = collection(db, "listings");
+      // create query
+      const q = query(
+        listingRef,
+        where("offer", "==", true),
+        orderBy("timestamp", "desc"),
+        startAfter(lastFetchListing),
+        limit(3)
+      );
+      // execute query
+      const querySnap = await getDocs(q);
+      const laseVisable = querySnap.docs[querySnap.docs.length - 1];
+      setLastFetchListing(laseVisable);
+      const listings = [];
+      querySnap.forEach((doc) => {
+        return listings.push({
+          id: doc.id,
+          data: doc.data(),
+        });
+      });
+      setListings((prevState) => [...prevState, ...listings]);
+      setLoading(false);
+    } catch (error) {
+      toast.error("Offer Could not Load..");
+    }
+  }
+
   return (
     <div className="max-w-6xl mx-auto px-3">
-      <h1 className="text-3xl text-center mt-6 font-bold">Offers</h1>
+      <h1 className="text-3xl text-center mt-6 font-bold mb-4">Offers</h1>
       {loading ? (
         <Spinner />
       ) : listings && listings.length > 0 ? (
@@ -64,6 +99,16 @@ export default function Offers() {
               ))}
             </ul>
           </main>
+          {lastFetchListing && (
+            <div className="flex justify-center items-center mb-6">
+              <button
+                onClick={onFetchMoreListings}
+                className="bg-white px-3 py-1.5 text-gray-700 border border-gray-300 mt-4 rounded hover:border-slate-600 transition duration-150 ease-in-out"
+              >
+                Load More
+              </button>
+            </div>
+          )}
         </>
       ) : (
         <p>There Are No Current Offers!</p>
